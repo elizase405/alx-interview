e87cb0fb99ebdca14f9ed70150bd6995f92e7c06
Elizabeth <elizase405@gmail.com>
2024-08-27 04:40:40 -0700

    feat: UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+""" validUTF8: determines if a given data set represents a valid UTF-8 encoding """
+
+def validUTF8(data):
+    """
+    Args:
+        data - a list of integers with each integer representing 1 byte of data
+    Return:
+        True if data is valid UTF-8 encoding, else return False
+    """
+
+    data_length = len(data)
+    for i in range(0, data_length):
+        if i <= 128:
+            continue
+        else:
+            return False
+    return True
