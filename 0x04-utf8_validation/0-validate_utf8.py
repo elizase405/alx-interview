032ead3c34144b549a499d9067bd6ee10e8c698b
Elizabeth <elizase405@gmail.com>
2024-09-25 18:46:14 +0000

    feat: task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,16 +10,37 @@ def validUTF8(data):
         data - a list of integers with each integer representing 1 byte of data
     Return:
         True if data is valid UTF-8 encoding, else return False
+
+    1-byte Sequence: 0xxxxxxx
+    2-byte Sequence: 110xxxxx 10xxxxxx
+    3-byte Sequence: 1110xxxx 10xxxxxx 10xxxxxx
+    4-byte Sequence: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+    0x80 is 10000000
+    0xC0 is 11000000
+    0xE0 is 11100000
+    0xF0 is 11110000
     """
 
-    for byte in data:
-        data_bit = format(byte, '08b')
-        if data_bit.startswith('0') \
-        or data_bit.startswith('10') and len(data_bit) == 8\
-        or data_bit.startswith('110') \
-        or data_bit.startswith('1110') \
-        or data_bit.startswith('11110'):
-            continue
+    i = 0
+    while i < len(data):
+        if data[i] & 0x80 == 0x00:
+            i += 1
+        elif data[i] & 0xC0 == 0xC0:
+            if i + 1 >= len(data) or data[i + 1] & 0x80 != 0x80:
+                return False
+            i += 2
+        elif data[i] & 0xE0 == 0xE0:
+            if i + 1 >= len(data) or data[i + 1] & 0x08 != 0x08 \
+                                  or data[i + 2] & 0x08 != 0x08:
+                return False
+            i += 3
+        elif data[i] & 0xF0 == 0xF0:
+            if i + 1 >= len(data) or data[i + 1] & 0x80 != 0x80 \
+                                  or data[i + 2] & 0x80 != 0x80 \
+                                  or data[i + 3] & 0x08 != 0x08:
+                return False
+            i += 4
         else:
             return False
     return True
