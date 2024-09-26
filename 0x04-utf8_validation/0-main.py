df010939722d3a717b890bfa4927cbae4fefaeb6
Elizabeth <elizase405@gmail.com>
2024-09-26 16:03:41 +0000

    fix: fix error

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -14,5 +14,5 @@ print(validUTF8(data))
 data = [229, 65, 127, 256]
 print(validUTF8(data))
 
-data = [467, 133, 108]
+data =  [240, 188, 128, 167]
 print(validUTF8(data))
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -26,19 +26,19 @@ def validUTF8(data):
     while i < len(data):
         if data[i] & 0x80 == 0x00:
             i += 1
-        elif data[i] & 0xC0 == 0xC0:
-            if i + 1 >= len(data) or data[i + 1] & 0x80 != 0x80:
+        elif data[i] & 0xE0 == 0xC0:
+            if i + 1 >= len(data) or data[i + 1] & 0xC0 != 0x80:
                 return False
             i += 2
-        elif data[i] & 0xE0 == 0xE0:
-            if i + 1 >= len(data) or data[i + 1] & 0x08 != 0x08 \
-                                  or data[i + 2] & 0x08 != 0x08:
+        elif data[i] & 0xF0 == 0xE0:
+            if i + 1 >= len(data) or data[i + 1] & 0xC0 != 0x80 \
+                                  or data[i + 2] & 0xC0 != 0x80:
                 return False
             i += 3
-        elif data[i] & 0xF0 == 0xF0:
-            if i + 1 >= len(data) or data[i + 1] & 0x80 != 0x80 \
-                                  or data[i + 2] & 0x80 != 0x80 \
-                                  or data[i + 3] & 0x08 != 0x08:
+        elif data[i] & 0xF8 == 0xF0:
+            if i + 1 >= len(data) or data[i + 1] & 0xC0 != 0x80 \
+                                  or data[i + 2] & 0xC0 != 0x80 \
+                                  or data[i + 3] & 0xC0 != 0x80:
                 return False
             i += 4
         else:
