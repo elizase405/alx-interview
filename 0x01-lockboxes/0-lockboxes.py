8cad06154863cc313580633af00ab19fa3111ac1
Elizabeth <elizase405@gmail.com>
2025-01-15 01:35:59 -0800

    feat: lockboxes interview question

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+    def canUnlockAll(boxes): Lockboxes function
+    canUnlockAll(boxes) - true
+"""
+
+from typing import List
+
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """
+        method to determine if all the boxes
+        can be opened.
+        - A key with the same number as a box opens that box.
+        - assume all keys are positive
+        - there can be keys that do not have boxes
+        - The first box boxes[0] is unlocked
+
+        Args: boxes(list of list) -
+            a list of boxes, where each box may
+            contain keys to the other boxes.
+
+        return: bool - True if all boxes
+                can be opened, else return False
+    """
+
+    keys = []
+
+    # Get the keys in the first box
+    for key in boxes[0]:
+        keys.append(key)
+
+    # Use the keys in the first box to open the other boxes.
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys:
+                keys.append(new_key)
+
+    keys_len = len(keys)
+    boxes_len = len(boxes)
+
+    if ((keys_len == (boxes_len - 1)) or (keys_len >= boxes_len)):
+        return True
+    return False
